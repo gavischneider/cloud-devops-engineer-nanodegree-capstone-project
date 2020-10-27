ba051450994e66c0969abc953455d641987effa2
Gavi Schneider <gavischneider@gmail.com>
2020-10-27 18:29:22 +0200

    Added MusixMatch integration

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,23 @@
 import express, {Application, Request, Response, NextFunction} from 'express';
+// import randomSong from '@chatandshare/random-song';
+const randomSong = require('@chatandshare/random-song');
 
 const app: Application = express();
 
+const random = new randomSong("5a6a3319d10b8dd4593f110d6db57172");
+
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send("Welcome to my capstone project");
+    res.send("Welcome to my capstone project!");
 });
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.get('/random', async (req: Request, res: Response, next: NextFunction) => {
+    
+ 
+    let very_random_song = await random.song();
+        
+    console.log(very_random_song); // Outputs a random song
+    res.send(very_random_song);
+    
+})
+
+app.listen(8080, () => console.log('Server running on port 8080'));
