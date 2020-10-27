1f386bfbcb3bc736f95881888253062cba1ea7f3
Gavi Schneider <gavischneider@gmail.com>
2020-10-27 17:03:07 +0200

    Created server

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,11 @@
 "use strict";
-let numbers = [1, 2, 3];
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+app.get('/', (req, res, next) => {
+    res.send("Welcome to my capstone project");
+});
+app.listen(5000, () => console.log('Server running on port 5000'));
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-let numbers: number[] = [1, 2, 3];
+import express, {Application, Request, Response, NextFunction} from 'express';
+
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send("Welcome to my capstone project");
+});
+
+app.listen(5000, () => console.log('Server running on port 5000'));
